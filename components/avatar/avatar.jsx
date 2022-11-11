bf9b57492f1512a7e1b85f05f71dbb29acc9a366
Yoelvys Perez Cabrera <yoelvyspc93@gmail.com>
2022-11-11 17:46:56 -0500

    add avatar component

---
+++ components/avatar/avatar.jsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import styles from './avatar.module.css'
+
+export default function Avatar ({ available, img, imgDimension, statusDimension, ...props }) {
+  const active = available ? '/asset/svg/badge-active.svg' : '/asset/svg/badge-desactive.svg'
+
+  return (
+    <div {...props}>
+      <div className={styles.img}>
+        <span className={styles.badge}>
+          <Image src={active} alt='badge available' width={statusDimension} height={statusDimension} />
+        </span>
+        <Image src={img} alt='avatar' width={imgDimension} height={imgDimension} />
+      </div>
+    </div>
+  )
+}
