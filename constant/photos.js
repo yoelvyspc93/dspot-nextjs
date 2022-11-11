8e87264abefa57a8dc4c464c64eb81700161fe5d
Yoelvys Perez Cabrera <yoelvyspc93@gmail.com>
2022-11-11 17:49:11 -0500

    add constants to mock data

---
+++ constant/photos.js
@@ -0,0 +1,13 @@
+const photos = [
+  '/asset/images/galery.png',
+  '/asset/images/galery.png',
+  '/asset/images/galery.png',
+  '/asset/images/galery.png',
+  '/asset/images/galery.png',
+  '/asset/images/galery.png',
+  '/asset/images/galery.png',
+  '/asset/images/galery.png',
+  '/asset/images/galery.png'
+]
+
+export default photos
