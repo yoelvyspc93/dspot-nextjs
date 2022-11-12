a2bd56b2e6bc5fef4cfbb72f1798274e80080efe
Yoelvys Perez Cabrera <yoelvyspc93@gmail.com>
2022-11-11 22:03:05 -0500

    add icon component (back arrow and close)

---
+++ components/icons/icons.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import styles from './icons.module.css'
+
+export function IconArrowBlack ({ onClick, ...props }) {
+  return (
+    <div {...props}>
+      <div className={`${styles.icon} ${styles.icon_arrow_black}`} onClick={onClick}>
+        <Image src='/asset/svg/arrow-black.svg' alt='icon-arrow-black' width={24} height={24} />
+      </div>
+    </div>
+  )
+}
+
+export function IconClose (props) {
+  return (
+    <div {...props}>
+      <span className={`${styles.icon} ${styles.icon_close}`}>
+        <Image src='/asset/svg/close.svg' alt='icon_close' width={24} height={24} />
+      </span>
+    </div>
+
+  )
+}
