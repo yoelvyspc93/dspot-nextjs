10c631644cd0a7fd59d6a872c14ce7ca8efdc08e
Yoelvys Perez Cabrera <yoelvyspc93@gmail.com>
2022-11-11 22:04:29 -0500

    add index to import components

---
+++ components/index.js
@@ -0,0 +1,22 @@
+import Button from 'components/botton/button'
+import Card from 'components/card/card'
+import List from 'components/list/List'
+import Avatar from 'components/avatar/Avatar'
+import Badge from 'components/badge/Badge'
+import Divider from 'components/divider/Divider'
+import DetailInfo from 'components/detail/info/info'
+import DetailPhotos from 'components/detail/photos/photos'
+import { IconArrowBlack, IconClose } from 'components/icons/icons'
+
+export {
+  Button,
+  Card,
+  List,
+  Avatar,
+  Badge,
+  DetailInfo,
+  DetailPhotos,
+  IconArrowBlack,
+  IconClose,
+  Divider
+}
