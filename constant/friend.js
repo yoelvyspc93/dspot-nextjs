fe20c338220d5d6bcf4485bd9290eaed4fcaa16b
Yoelvys Perez Cabrera <yoelvyspc93@gmail.com>
2022-11-13 16:07:51 -0500

    new contantes

---
+++ constant/friend.js
@@ -0,0 +1,29 @@
+const friend = {
+  id: 6,
+  img: '/asset/images/avatar.png',
+  first_name: 'Steph',
+  last_name: 'Walters',
+  phone: '(820) 289-1818',
+  address_1: '5190 Center Court Drive',
+  city: 'Spring',
+  state: 'TX',
+  zipcode: '77370',
+  bio: "I'm very choosy. I'm also very suspicious, very irrational and I have a very short temper. I'm also extremely jealous and slow to forgive. Just so you know.",
+  photos: [
+    '/asset/images/galery.png',
+    '/asset/images/galery.png',
+    '/asset/images/galery.png',
+    '/asset/images/galery.png'
+  ],
+  statuses: [
+    'Developing something amazing',
+    'This could be interesting....',
+    'Man, life is so good',
+    'There is nothing quite like a good friend',
+    'Take a look around you, everything is awesome',
+    'What is the point of all of this'
+  ],
+  available: true
+}
+
+export default friend
--- constant/frineds.js
+++ constant/frineds.js
@@ -1,4 +1,4 @@
-const frineds = [
+const friends = [
   {
     id: 1,
     img: '/asset/images/avatar.png',
@@ -97,4 +97,55 @@ const frineds = [
   }
 ]
 
-export default frineds
+const friendsApi = [
+  {
+    id: 1,
+    img: 'https://s3.amazonaws.com/uifaces/faces/twitter/csswizardry/128.jpg',
+    first_name: 'Jeremy',
+    last_name: 'Davis',
+    status: 'At work...',
+    available: false
+  },
+  {
+    id: 2,
+    img: 'https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg',
+    first_name: 'Vlad',
+    last_name: 'Baratovich',
+    status: 'Hangout out by the pool',
+    available: true
+  },
+  {
+    id: 3,
+    img: 'https://s3.amazonaws.com/uifaces/faces/twitter/rssems/128.jpg',
+    first_name: 'Reese',
+    last_name: 'Samsonite',
+    status: 'At NG-conf!',
+    available: true
+  },
+  {
+    id: 4,
+    img: 'https://s3.amazonaws.com/uifaces/faces/twitter/enda/128.jpg',
+    first_name: 'Edwardo',
+    last_name: 'Gonzalez',
+    status: 'Developing something amazing',
+    available: false
+  },
+  {
+    id: 5,
+    img: 'https://s3.amazonaws.com/uifaces/faces/twitter/jm_denis/128.jpg',
+    first_name: 'Jim',
+    last_name: 'Denison',
+    status: 'Designing beatiful things',
+    available: false
+  },
+  {
+    id: 6,
+    img: 'https://s3.amazonaws.com/uifaces/faces/twitter/walterstephanie/128.jpg',
+    first_name: 'Steph',
+    last_name: 'Walters',
+    status: 'Developing something amazing',
+    available: true
+  }
+]
+
+export default friends
