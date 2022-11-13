619765621856026f43c08d513688d5f7d25c3aa1
Yoelvys Perez Cabrera <yoelvyspc93@gmail.com>
2022-11-13 16:09:13 -0500

    component index

--- components/index.js
+++ components/index.js
@@ -1,22 +1,28 @@
 import Button from 'components/botton/button'
+import Typography from 'components/typography/typography'
 import Card from 'components/card/card'
-import List from 'components/list/List'
-import Avatar from 'components/avatar/Avatar'
-import Badge from 'components/badge/Badge'
-import Divider from 'components/divider/Divider'
+import List from 'components/list/list'
+import Avatar from 'components/avatar/avatar'
+import Badge from 'components/badge/badge'
+import Photo from 'components/photo/photo'
+import Divider from 'components/divider/divider'
+import DetailHeader from 'components/detail/header/header'
 import DetailInfo from 'components/detail/info/info'
 import DetailPhotos from 'components/detail/photos/photos'
 import { IconArrowBlack, IconClose } from 'components/icons/icons'
 
 export {
   Button,
+  Typography,
   Card,
   List,
   Avatar,
   Badge,
+  DetailHeader,
   DetailInfo,
   DetailPhotos,
   IconArrowBlack,
   IconClose,
+  Photo,
   Divider
 }
