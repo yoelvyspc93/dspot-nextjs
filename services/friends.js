dc940b85951fa87221b3975ead21ccb76765c6b2
Yoelvys Perez Cabrera <yoelvyspc93@gmail.com>
2022-11-14 17:35:08 -0500

    change the services by the real url

--- services/friends.js
+++ services/friends.js
@@ -1,21 +1,50 @@
-import friend from 'constant/friend'
-import friends from 'constant/frineds'
-
-/* const API_URL = process.env.API_URL */
-
-function getFriendsAll () {
-  /* const res = await fetch(`${API_URL}/friends`, { cache: 'no-store' })
-  return res.json() */
-  return friends
-}
-
-function getFriendById ({ id }) {
-  /* const res = await fetch(`${API_URL}/friends/${id}`)
-  return res.json() */
-  return friend
-}
+const API_URL = process.env.API_URL
 
 export {
   getFriendsAll,
   getFriendById
+}
+
+/**
+ * Service to retrieve a list of friend objects
+ */
+async function getFriendsAll () {
+  const res = await fetch(`${API_URL}/friends`, { cache: 'no-store' })
+  const data = await res.json()
+  return mapOnePhotoFriendsAll(data)
+}
+
+/**
+ * Service to retrieve a friend object by its ID
+ * @param { id: number } friend ID
+ */
+async function getFriendById ({ id }) {
+  const res = await fetch(`${API_URL}/friends/id`, { cache: 'no-store' })
+  const data = await res.json()
+  return mapOnePhotoFriendById(data)
+}
+
+/* These 2 methods are temporary because it does not load the
+photo from s3.amazonaws.com */
+
+function mapOnePhotoFriendsAll (lists) {
+  return lists.map(obj => {
+    return {
+      ...obj,
+      img: '/asset/images/avatar.png'
+    }
+  })
+}
+
+function mapOnePhotoFriendById (data) {
+  return {
+    ...data,
+    img: '/asset/images/avatar.png',
+    photos: [
+      '/asset/images/galery.png',
+      '/asset/images/galery.png',
+      '/asset/images/galery.png',
+      '/asset/images/galery.png'
+    ]
+  }
 }
